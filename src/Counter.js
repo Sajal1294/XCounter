db73252309db2377ec9b60d2a36bf9ab3b1f7015
Sajal Mandal <sajal1294@gmail.com>
2025-04-24 12:42:58 +0530

    first commit

---
+++ src/Counter.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+
+function CounterApp(){
+
+    const [count,setCount]=useState(0);
+
+    return(
+    
+    <div>
+        <h1>Counter App</h1>
+        <p>
+            Counter: {count}
+        </p>
+        <button onClick={()=>{setCount(count+1)}}>Increment</button>
+        <button onClick={()=>{setCount(count-1)}}>Decrement</button>
+
+
+    </div>
+
+    );
+}
+
+export default CounterApp;
